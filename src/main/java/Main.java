fc6767ca34cdaa856f3305d0888c76fd46ab00de
Lawrence Najim <najimlawr@myvuw.ac.nz>
2021-12-30 21:25:51 +1300

    Main method - This runs the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,12 @@
 public class Main{
 
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter a number from 1-10:");
+    }
+
+
 }
