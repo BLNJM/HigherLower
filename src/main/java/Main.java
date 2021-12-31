d42f832838c2dfd3af3f403aa5b2e171c33e3cd7
Lawrence Najim <najimlawr@myvuw.ac.nz>
2021-12-31 22:50:57 +1300

    Reordered logic, can now generate necessary numbers and ask for user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,37 @@
-public class Main{
+import java.util.Scanner;
 
-    // global values for game numbers
-    private int playerNum;
-    private int cpuNum;
+public class Main{
 
     /**
      *
      * @param args none
      */
     public static void main(String[] args) {
-        System.out.println("Enter a number from 1-10:");
+        Scanner scanner = new Scanner(System.in);
+        boolean play = true;
+
+        while (play == true) {
+            int firstNum = generateNum();
+
+            System.out.println("Number is " + firstNum + ", will the next number be higher or lower? (numbers range from 1-20)");
+
+            String answer = scanner.next();
+            while (!answer.equalsIgnoreCase("higher") && !answer.equalsIgnoreCase("lower")) {
+                System.out.println("invalid response, try again");
+            }
+
+            int secondNum = generateNum();
+
+            //if (answer.equalsIgnoreCase("higher") && ) {}
+        }
+        scanner.close();
     }
 
     /**
      * Method to generate a number
      * @return generated integer
      */
-    private int generateNum() {
+    private static int generateNum() {
         return 0;
     }
 }
