754064ec4320d4e03847151a1c7efe83bdff3936
Lawrence Najim <najimlawr@myvuw.ac.nz>
2022-01-03 16:43:01 +1300

    Continuously asks for user input until said input is allowed

--- src/HigherLower.java
+++ src/HigherLower.java
@@ -10,23 +10,16 @@ public class HigherLower{
         Scanner scanner = new Scanner(System.in);
         boolean play = true;
 
-        while (play == true) {
-            int firstNum = generateNum();
+        int firstNum = generateNum();
 
-            System.out.println("Number is " + firstNum + ", will the next number be higher or lower? (numbers range from 1-20)");
+        System.out.println("Number is " + firstNum + ", will the next number be higher or lower? (numbers range from 1-20):");
 
-            String answer = scanner.next();
-            if (!answer.equalsIgnoreCase("higher") && !answer.equalsIgnoreCase("lower")) {
-                System.out.println("invalid response, try again");
-            }
-            else {
-                int secondNum = generateNum();
-            }
-
-            break;
-
-            //if (answer.equalsIgnoreCase("higher") && ) {}
+        while (!scanner.next().equalsIgnoreCase("higher") && !scanner.next().equalsIgnoreCase("lower")) {
+            System.out.println("invalid response, try again");
         }
+
+        //if (answer.equalsIgnoreCase("higher") && ) {}
+
         scanner.close();
     }
 
