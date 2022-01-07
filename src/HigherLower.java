75cb57c8366d372d707e77968afb2bd8e4930f0d
Lawrence Najim <najimlawr@myvuw.ac.nz>
2022-01-07 16:12:57 +1300

    Bugfix for double line during scanner input

--- src/HigherLower.java
+++ src/HigherLower.java
@@ -8,15 +8,20 @@ public class HigherLower{
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String answer = "";
 
         int firstNum = generateNum();
 
         System.out.println("Number is " + firstNum + ", will the next number be higher or lower? (numbers range from 1-20):");
 
-        while (!scanner.next().equalsIgnoreCase("higher") && !scanner.next().equalsIgnoreCase("lower")) {
-            System.out.println("invalid response, try again");
+        while (!answer.equalsIgnoreCase("higher") && !answer.equalsIgnoreCase("lower")) {
+            answer = scanner.next();
+            if (!answer.equalsIgnoreCase("higher") && !answer.equalsIgnoreCase("lower")) {
+                System.out.println("invalid response, try again");
+            }
         }
 
+        System.out.println("valid response");
         //if (answer.equalsIgnoreCase("higher") && ) {}
 
         scanner.close();
